a347b5ff56e8049d92d6bda6a2c083096d232095
Dylan Martin <119099353+DylanMartino@users.noreply.github.com>
2023-09-06 12:25:44 +1200

    Add files via upload

---
+++ COMP609-Assessment2_ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP609_Assessment2_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

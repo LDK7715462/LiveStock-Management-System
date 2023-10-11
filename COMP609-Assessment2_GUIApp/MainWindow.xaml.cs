b14c7d4c20c25c0ca2930f0e4f46ef0fac1198df
SandalBandit <119099353+SandalBandit@users.noreply.github.com>
2023-10-12 12:17:38 +1300

    Working GUI and Menus

--- COMP609-Assessment2_GUIApp/MainWindow.xaml.cs
+++ COMP609-Assessment2_GUIApp/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using COMP609_Assessment2_GUIApp.Views;
+﻿using COMP609_Assessment2_GUIApp.Pages;
 using COMP609_Assessment2_GUIApp.Models;
 using System;
 using System.Collections.Generic;
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,23 +24,159 @@ namespace COMP609_Assessment2_GUIApp
     public partial class MainWindow : Window
     {
         LMSApp app;
-
         public MainWindow()
         {
             InitializeComponent();
             app = new LMSApp();
         }
 
-        private void TabControl_selectionChanged(object sender, SelectionChangedEventArgs e)
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            int index = TabMenu.SelectedIndex;
-            MainView.Content = index switch
+            Tg_Btn.IsChecked = false;
+        }
+
+        // Start: MenuLeft PopupButton //
+        private void btnHome_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
             {
-                0 => new TestPage(app),
-                1 => new TestPage(app),
-                2 => new TestPage(app),
-                _ => throw new NotImplementedException(),
-            };
+                Popup.PlacementTarget = btnHome;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Home";
+            }
+        }
+
+        private void btnHome_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnDashboard_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnDashboard;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Dashboard";
+            }
+        }
+
+        private void btnDashboard_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnQueryData_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnQueryData;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Query Data";
+            }
+        }
+
+        private void btnQueryData_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnStatistics_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnStatistics;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Statistics";
+            }
+        }
+
+        private void btnStatistics_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnUpdateData_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnUpdateData;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = "Update Data";
+            }
+        }
+
+        private void btnUpdateData_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+        // End: MenuLeft PopupButton //
+
+        // Start: Button Close | Restore | Minimize 
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        // End: Button Close | Restore | Minimize
+
+        //private void btnHome_Click(object sender, RoutedEventArgs e)
+        //{
+        //    fContainer.Navigate(new System.Uri("Pages/Home.xaml", UriKind.RelativeOrAbsolute));
+        //}
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Content = new Home();
+        }
+
+        private void btnDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Content = new TestPage(app);
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LightThemeClick(object sender, RoutedEventArgs e)
+        {
+            AppTheme.ChangeTheme(new Uri("Themes/Light.xaml", UriKind.Relative));
+        }
+
+        private void DarkThemeClick(object sender, RoutedEventArgs e)
+        {
+            AppTheme.ChangeTheme(new Uri("Themes/Dark.xaml", UriKind.Relative));
         }
     }
 }
